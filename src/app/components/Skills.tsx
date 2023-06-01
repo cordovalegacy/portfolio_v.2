f1cc04c1aa42f85f70f94dafe8716a4bada5b1a9
Brendan Cordova <cordovalegacy19@gmail.com>
2023-06-01 09:30:28 -0700

    Done with Experience Page

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,36 +1,42 @@
 // !Components
 import BarChart from './BarChart'
+import AnimatedText from './AnimatedText'
 
 // !ChartJS Imports
 import {
     Chart as ChartJS,
-    ArcElement,
     Tooltip,
     Legend,
     Title,
     CategoryScale
-} from "chart.js/auto";
+} from "chart.js/auto"
 import 'chartjs-plugin-datalabels'
 
 // !ChartJS Config
 ChartJS.register(
-    ArcElement,
     Tooltip,
     Legend,
     Title,
     CategoryScale // Register CategoryScale for "category" scale
-);
+)
 
 // !ChartJS Data
-const labels = ['Languages', 'Frameworks', 'ORM', 'Databases', 'Tools']
+const labels = ['React', 'JavaScript', 'Python', 'TypeScript', 'JQuery', 'CSS', 'Tailwind', 'SASS', 'Bootstrap', 'NextJS', 'NodeJS', 'Flask', 'Express', 'MongoDB', 'MySQL', 'Git', 'Miro', 'etc..']
 const data = {
     labels: labels,
     datasets: [{
         axis: 'y',
-        label: 'Skill',
-        data: [65, 59, 80, 91, 56, 55, 40],
+        label: 'Proficiency (out of 100)',
+        data: [85, 85, 70, 70, 60, 90, 90, 70, 80, 75, 85, 70, 85, 80, 70, 90, 70, 100],
         fill: false,
         backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
             'rgba(255, 99, 132, 0.2)',
             'rgba(255, 159, 64, 0.2)',
             'rgba(255, 205, 86, 0.2)',
@@ -46,46 +52,71 @@ const data = {
             'rgb(75, 192, 192)',
             'rgb(54, 162, 235)',
             'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
             'rgb(201, 203, 207)'
         ],
-        borderWidth: 1
+        borderWidth: 3
     }]
-};
+}
 
 // !ChartJS Options
-var options = {
+const options = {
     responsive: true,
-    title: {
-        display: true,
-        position: "top",
-        text: "Bar Graph",
-        fontSize: 25,
-        fontColor: "#111"
-    },
-    legend: {
-        display: true,
-        position: "bottom",
-        labels: {
-            fontColor: "#333",
-            fontSize: 18
-        }
+    maintainAspectRatio: false,
+    plugins: {
+        title: {
+            display: true,
+            position: 'top',
+            text: 'Languages, Frameworks, Databases, Tools, and More',
+            font: {
+                size: 25,
+                color: '#111',
+            },
+        },
+        legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+                color: '#333',
+                font: {
+                    size: 18,
+                },
+            },
+        },
     },
     scales: {
-        yAxes: [{
+        x: {
+            stacked: true,
             ticks: {
-                min: 0
-            }
-        }]
+                font: {
+                    weight: 'bold',
+                },
+            },
+        },
+        y: {
+            stacked: true,
+        },
+    },
+    animation: {
+        duration: 3000,
     }
-};
+}
+
+
 
 const Skills = () => {
 
     return (
-        <>
-            <h2 className="h-full font-bold text-6xl w-full text-center">Skills</h2>
+        <div className='h-full w-full '>
+            <AnimatedText text={"Skill Sets"} className='text-5xl text-center' />
             <BarChart data={data} options={options} />
-        </>
+        </div>
     )
 }
 
